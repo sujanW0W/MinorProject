a2b883ff0a4a5310227bbe58fabb9f6d9367a96b
Shady <96483067+Nibendra@users.noreply.github.com>
2022-02-24 17:22:29 +0545

    Add files via upload

---
+++ jrsc/jrsc/src/main/java/com/HomeDecor/jrsc/appUser/ClientRepository.java
@@ -0,0 +1,14 @@
+package com.HomeDecor.jrsc.appUser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ClientRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+
+}
---
+++ jrsc/jrsc/src/main/java/com/HomeDecor/jrsc/appUser/Gender.java
@@ -0,0 +1,7 @@
+package com.HomeDecor.jrsc.appUser;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
