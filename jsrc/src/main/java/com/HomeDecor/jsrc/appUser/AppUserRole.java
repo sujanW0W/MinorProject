ab1e0d1fcaa426b2f461b4f4e42a3938f8ae40a1
am.0007 <96876023+am-0007@users.noreply.github.com>
2022-02-25 21:12:03 +0545

    Delete jsrc directory

--- jsrc/src/main/java/com/HomeDecor/jsrc/appUser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.HomeDecor.jsrc.appUser;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- jsrc/src/main/java/com/HomeDecor/jsrc/appUser/Gender.java
+++
@@ -1,7 +0,0 @@
-package com.HomeDecor.jsrc.appUser;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
